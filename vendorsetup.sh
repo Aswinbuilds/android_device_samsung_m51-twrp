fbb35206595357bf28f006a88abc13d1e8508526
Aswin <73487313+aswin11234@users.noreply.github.com>
2022-04-21 17:18:41 +0800

    twrp bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m51-user
-add_lunch_combo omni_m51-userdebug
-add_lunch_combo omni_m51-eng
+add_lunch_combo twrp_m51-user
+add_lunch_combo twrp_m51-userdebug
+add_lunch_combo twrp_m51-eng
+
+#CCACHE
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/cache
+ccache -M 15G
