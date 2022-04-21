a6fcf5e7511df8dd302a6b0ab72d0314550f2677
Aswin <73487313+aswin11234@users.noreply.github.com>
2022-04-21 18:01:30 +0800

    twrp bringup

--- twrp_m51.mk
+++ twrp_m51.mk
@@ -6,20 +6,29 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from m51 device
 $(call inherit-product, device/samsung/m51/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m51
-PRODUCT_NAME := omni_m51
+PRODUCT_NAME := twrp_m51
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M515F
 PRODUCT_MANUFACTURER := samsung
+
+# Build info
+BUILD_FINGERPRINT := "samsung/m51nsxx/m51:11/RP1A.200720.012/M515FXXS3CUI2:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=m51 \
+    PRODUCT_NAME=m51 \
+    PRIVATE_BUILD_DESC="m51nsxx-user-11-RP1A.200720.012-M515FXXS3CUI2-release-keys."
