6229ee7d43c84b05ce2bf16104a663c29368730d
Aswin <73487313+aswin11234@users.noreply.github.com>
2022-04-22 16:44:49 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ PRODUCT_PLATFORM := sm6150
 # Define Dynamic Partition support
 PRODUCT_BUILD_SUPER_PARTITION := false
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
